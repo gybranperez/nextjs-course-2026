fdb49b6a69da4f0be205f2bc9df3e2859c0063aa
gybranperez <gybranperez@gmail.com>
2026-02-03 14:30:20 -0600

    Curso react components server y client

---
+++ 001webapp/first-steps/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <>
+            <span className="text-7xl align-middle text-shadow-amber-50 text-shadow-2xs">About</span>
+        </>
+    )
+}
